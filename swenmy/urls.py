c9571636151781abcdb972318c7a12d61cc2c28e
Yashfa83 <yashfaejaz83@gmail.com>
2026-06-29 12:46:49 -0700

    product order and payments

--- swenmy/urls.py
+++ swenmy/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('carts/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    # orders
+    path('orders/', include('orders.urls')),
 ]+ static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
+
     
 
